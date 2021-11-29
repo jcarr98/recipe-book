a805a07906977c2dd43ab88641299162a7492ec6
Jeffrey Carr <jeff@Jeffs-Computer.fios-router.home>
2021-11-29 11:31:42 -0500

    Removed unused function

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,14 +1,6 @@
 import { Button } from 'grommet';
 
-import Axios from 'axios';
-
 export default function Login() {
-    function go() {
-        Axios.get('https://jeans-recipe-book.herokuapp.com/api/auth').then((data) => {
-            console.log(data.data);
-        });
-    }
-
     return(
         <Button primary href="https://jeans-recipe-book.herokuapp.com/api/auth" label="Login" />
     );
